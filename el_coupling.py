af04c7394d18ab98e890bf09122275f43cc9cae5
Francesco Leccese <francescoleccese4@gmail.com>
2026-05-06 14:59:24 +0200

    P_term modified

--- el_coupling.py
+++ el_coupling.py
@@ -28,53 +28,66 @@ class ElectronicCoupling:
         
 		return j_exch
 
-	def get_P_term(self):
+	def get_P_term(self, singlet=True):
+
+		#Supermolecule approach
+		mol_super = gto.mole.conc_mol(self.mol_D, self.mol_A)
+		nD , nA = self.mol_D.nao , self.mol_A.nao
         
-        # 1. Overlap integrals (Only between D and A) s_cross is a matrix
-		s_cross = gto.mole.intor_cross('int1e_ovlp', self.mol_D, self.mol_A)
-        
-        # 2. HOMO (h) and LUMO (l) coefficients
-		nocc = self.mol_D.nelectron // 2
-		c_d_h = self.m1.mf.mo_coeff[:, nocc-1]
-		c_d_l = self.m1.mf.mo_coeff[:, nocc]
-		c_a_h = self.m2.mf.mo_coeff[:, nocc-1]
-		c_a_l = self.m2.mf.mo_coeff[:, nocc]
+		#HOMO(h) and LUMO(l) coefficients assuming a closed shell system
+		nocc_d = self.mol_D.nelectron // 2
+		c_d_h = self.m1.mf.mo_coeff[:, nocc_d-1]
+		c_d_l = self.m1.mf.mo_coeff[:, nocc_d]
 
-        # 3. S_hh e S_ll (scalar numbers)
-		s_ab = c_d_h @ s_cross @ c_a_h
-		s_apbp = c_d_l @ s_cross @ c_a_l
-		
-		# 4. h term (Hamiltonian/Fock)
-		#dm_core_d = self.m1.mf.make_rdm1() - np.outer(c_d_h, c_d_h) * 2 #core density matrix with all donor electrons except for HOMO
-		dm_core_a = self.m2.mf.make_rdm1() - np.outer(c_a_h, c_a_h) #same for acceptor
-		
-		h_core_cross = gto.mole.intor_cross('int1e_kin', self.mol_D, self.mol_A) + gto.mole.intor_cross('int1e_nuc', self.mol_D, self.mol_A) #h_ij^sigma_pi
-		
-		vj_a, vk_a = get_jk((self.mol_D, self.mol_D, self.mol_A, self.mol_A), (dm_core_a,dm_core_a), scripts=('ijkl,kl->ij','ijkl,jk->il'))
+		nocc_a = self.mol_A.nelectron // 2
+		c_a_h = self.m2.mf.mo_coeff[:, nocc_a-1]
+		c_a_l = self.m2.mf.mo_coeff[:, nocc_a]
 
-		v_screen_a= (2* vj_a - vk_a)
-		h_aa = c_d_h @ (h_core_cross + v_screen_a) @ c_d_h
-		h_apap = c_d_l @ (h_core_cross + v_screen_a) @ c_d_l
+		c_a_super = np.concatenate([c_d_h, np.zeros(nA)])
+		c_ap_super = np.concatenate([c_d_l, np.zeros(nA)])
+		c_b_super = np.concatenate([np.zeros(nD), c_a_h])
+		c_bp_super = np.concatenate([np.zeros(nD), c_a_l])
 
-		h_ab = c_d_h @ (h_core_cross + v_screen_a) @ c_a_h
-		h_apbp = c_d_l @ (h_core_cross + v_screen_a) @ c_a_l
+        # Overlap integrals
+		s_super = mol_super.intor('int1e_ovlp')
+		s_ab = c_a_super @ s_super @ c_b_super
+		s_apbp = c_ap_super @ s_super @ c_bp_super
+		
+		# Core density matrices
+		dm_core_d = self.m1.mf.make_rdm1() - 2 * np.outer(c_d_h, c_d_h)
+		dm_core_a = self.m2.mf.make_rdm1() - 2 * np.outer(c_a_h, c_a_h)
+		dm_core_super = np.zeros((nD + nA, nD + nA))
+		dm_core_super[:nD, :nD] = dm_core_d
+		dm_core_super[nD:, nD:] = dm_core_a
+		
+		#Screened hamiltonian
+		h_core_super = mol_super.intor('int1e_kin') + mol_super.intor('int1e_nuc')
+		
+		vj_core, vk_core = scf.hf.get_jk(mol_super, dm_core_super)
+		h_eff = h_core_super + (2 * vj_core - vk_core)
+
+		h_aa = c_a_super @ h_eff @ c_a_super
+		h_apap = c_ap_super @ h_eff @ c_ap_super
+		h_ab = c_a_super @ h_eff @ c_b_super
+		h_apbp = c_ap_super @ h_eff @ c_bp_super
 	
-		# 5. Beta (beta_ab = h_ab - (s_ab * h_aa))
+		# Beta (beta_ab = h_ab - (s_ab * h_aa))
 		beta_ab = h_ab - (s_ab * h_aa)
 		beta_apbp = h_apbp - (s_apbp * h_apap)
 		
-		# 6. 2 electron integrals calculation via get_jk (in order to not save ERI in the RAM/disk)
-        #    V2e = (L_D L_D | H_A H_A) 
-		rho_ld = np.outer(c_d_l, c_d_l)
-		rho_ha = np.outer(c_a_h, c_a_h)
-		vj_v2e = get_jk((self.mol_A, self.mol_A, self.mol_D, self.mol_D), rho_ld, scripts='ijkl,ji->kl', aosym='s4')
-		v_2e = np.sum(vj_v2e * rho_ha)
+		# 2 electron integrals calculation via get_jk (in order to not save ERI in the RAM/disk)
+        # V2e = (L_D L_D | H_A H_A) 
+		rho_ap = np.outer(c_d_l, c_d_l)
+		rho_b = np.outer(c_a_h, c_a_h)
+		vj_v2e = get_jk((self.mol_A, self.mol_A, self.mol_D, self.mol_D), rho_ap, scripts='ijkl,ji->kl', aosym='s4')
+		v_2e = np.sum(vj_v2e * rho_b)
 		
         # 7. J0 = (L_D H_D | H_D L_D) -> Simile a K_Exchange
 		rho_t = np.outer(c_d_l, c_d_h)
 		v_j0 = get_jk(self.mol_D, rho_t, scripts='ijkl,ji->kl', aosym='s8')
 		j0 = np.sum(v_j0 * rho_t)
+		j0_term = j0 if singlet else -j0
 
-		p_term = (s_apbp * beta_ab) + (s_ab * beta_apbp) - (s_ab * s_apbp * (v_2e + j0))
+		p_term = (s_apbp * beta_ab) + (s_ab * beta_apbp) - (s_ab * s_apbp * (v_2e + j0_term))
         
 		return p_term * 27.2114 # eV
